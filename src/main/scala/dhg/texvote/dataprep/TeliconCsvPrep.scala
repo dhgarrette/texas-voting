2682e1505d2b369c06325cce286e5e44d642858f
Dan Garrette <dhgarrette@gmail.com>
2012-09-14 16:16:34 -0500

    produce a csv with legislator info and voting

--- src/main/scala/dhg/texvote/dataprep/TeliconCsvPrep.scala
+++ src/main/scala/dhg/texvote/dataprep/TeliconCsvPrep.scala
@@ -2,9 +2,12 @@ package dhg.texvote.dataprep
 
 import opennlp.scalabha.util.CollectionUtil._
 import opennlp.scalabha.util.CollectionUtils._
+import opennlp.scalabha.util.FileUtils
 import opennlp.scalabha.util.FileUtils._
 import java.io.File
-import opennlp.scalabha.util.FileUtils
+import au.com.bytecode.opencsv.CSVWriter
+import java.io.BufferedWriter
+import java.io.FileWriter
 
 object TeliconCsvPrep {
 
@@ -33,8 +36,7 @@ object TeliconCsvPrep {
         //        val memnum = "41"
         //        val session = "74R"
 
-        //        println(memnum)
-        //        println(session)
+        // println("%s %s".format(memnum, session))
 
         val legislatorPage = getLegislatorPage(memnum, session)
 
@@ -45,6 +47,8 @@ object TeliconCsvPrep {
             val (chamber, shift) = legislatorPage(3) match { case "House of" => ("House", 0); case "Senate" => ("Senate", -1) }
             val NameRe(name, party) = legislatorPage(6 + shift)
 
+            val votes = getVotesPage(memnum, session)
+
             Some(
               memnum,
               session,
@@ -54,33 +58,42 @@ object TeliconCsvPrep {
                 "chamber" -> Some(chamber),
                 "dob" -> findInfo("Date of Birth", legislatorPage).map(normalizeDate),
                 "degrees" -> findInfo("Degrees", legislatorPage),
-                "church" -> findInfo("Church Affiliation", legislatorPage)))
+                "church" -> findInfo("Church Affiliation", legislatorPage)),
+              votes)
 
           case "Unknown Member" => None
         }
       }
 
-    data.flatten.map(_._3).groupBy(_.split(" ").last).mapVals(_.toSet).toVector.sortBy(_._1).filter { case (k, vs) => vs.size > 1 } foreach println
+    data.flatten.map(_._3).groupBy(n => getLastName(n)).mapVals(_.toSet).toVector.sortBy(_._1).filter { case (k, vs) => vs.size > 1 } foreach println
 
     val byName = data.flatten
       .groupBy(_._3)
       .mapVals { entries =>
         val allInfo = entries.map(_._4)
-        val sessionsByMemnum = entries.map { case (memnum, session, _, _) => (memnum, session) }.groupByKey
-        val info = allInfo.flatMap(_.keySet).mapTo(k => allInfo.map(_(k))).toMap
-        (sessionsByMemnum, info)
+        val sessionsByMemnum = entries.map { case (memnum, session, _, _, _) => (memnum, session) }.groupByKey
+        val info = allInfo.flatMap(_.keys).mapTo(k => allInfo.map(_(k))).toMap
+        val votes = entries.map(_._5).flatten.ungroup.map { case (bill, (motion, vote)) => (bill + " - " + motion, vote) }.toMap
+        (sessionsByMemnum, info, votes)
       }
 
-    FileUtils.writeUsing("data/clean/legislator_pages.txt") { f =>
-      for ((name, (sessionsByMemnum, info)) <- byName.toVector.sortBy(n => (n._1.split(" ").last, n._1))) {
-        f.write("%s %s\n".format(name, sessionsByMemnum))
-        for (item <- Vector("party", "chamber", "dob", "degrees", "church"))
-          f.write("%s: %s\n".format(item, info(item).map(_.getOrElse("-None-"))))
-        f.write("\n")
+    val BillNameSortCriteriaRe = """(.{3}) - (.{2}.?) (\d+)\t.* - (.+) (.*)\t.*""".r
+    val voteNames = byName.flatMap(_._2._3.keys).toSet.toVector.sortBy {
+      case BillNameSortCriteriaRe(session, billType, billNum, motionType, motionId) =>
+        (session, billType, billNum.toInt, motionType, motionId)
+    }
+    //FileUtils.writeUsing("data/allVoteNames.txt") { f => voteNames.foreach(s => f.write(s + "\n")) }
+
+    val infoItems = Vector("party", "chamber", "dob", "degrees", "church")
+    FileUtils.using(new CSVWriter(new BufferedWriter(new FileWriter("data/clean/voting_data.csv")))) { f =>
+      f.writeNext((("name" +: "member numbers and sessions"  +: infoItems) ++ voteNames).toArray)
+      for ((name, (sessionsByMemnum, info, votes)) <- byName.toVector.sortBy(n => (getLastName(n._1), n._1))) {
+        val sessions = sessionsByMemnum.map{case (k,v) => "%s -> [%s]".format(k,v.toVector.sorted.mkString(",")) }.mkString(", ")
+        val items = infoItems.map(item => info(item).toVector.sorted.map(_.getOrElse("-None-")).mkString("; "))
+        val voteItems = voteNames.map(votes.getOrElse(_, ""))
+        f.writeNext(((name +: sessions +: items) ++ voteItems).toArray)
       }
     }
-
-    //readLines("data/scraped/votes_pages/%03d_%s.txt".format(memnum, session)).toVector
 
   }
 
@@ -101,6 +114,46 @@ object TeliconCsvPrep {
     x1
   }
 
+  /**
+   * @return Map[BillName, Map[MotionName, Vote]]
+   */
+  def getVotesPage(memnum: String, session: String) = {
+    val BillLineRe = """  ((HB|SB|HR|SR|HCR|SCR|HJR|SJR) \d+\t.*)""".r
+    val VoteLineRe = """ \t([YNAEPCX])\t[YN]\t\d\d/\d\d/\d\d\t(.+)""".r
+
+    val page = readLines(votesDir + "%03d_%s.txt".format(memnum.toInt, session)).toVector
+
+    if (page(0) == "Invalid Parameters")
+      Map[String, Map[String, String]]()
+
+    else {
+      val headerLines = page.indexOf(" \tVote	Maj	Date	Number	Vote Description") + 1
+      assert(page.size > 12, "In [%s %s]".format(memnum, session))
+      assert(page(page.length - 12) == " \t Votes 	 	 Percent \t ", page(page.length - 12))
+
+      page.drop(headerLines).dropRight(12).foldLeft(
+        List[(String, Map[String, String])]()) {
+          case (((curBill, votes) :: z), line) =>
+            line match {
+              case BillLineRe(bill, _) =>
+                (session + " - " + bill, Map[String, String]()) :: (curBill, votes) :: z
+              case VoteLineRe(vote, motion) =>
+                (curBill, votes.updated(motion, vote)) :: z
+            }
+          case (Nil, BillLineRe(bill, _)) =>
+            (session + " - " + bill, Map[String, String]()) :: Nil
+        }.toMap
+    }
+  }
+
+  def getLastName(name: String): String = {
+    val x0 = if (name.endsWith(".")) name.dropRight(1) else name
+    val x1 = if (x0.toLowerCase.endsWith(" jr")) x0.dropRight(3) else x0
+    val x2 = if (x1.toLowerCase.endsWith(" iii")) x1.dropRight(3) else x1
+    val x3 = if (x2.endsWith(",")) x2.dropRight(1) else x2
+    x3.split(" ").last
+  }
+
   def normalizeName(name: String) = {
     name match {
       case "Alma A. Allen" => "Alma Allen"
@@ -119,12 +172,14 @@ object TeliconCsvPrep {
       case "David H. Cain" => "David Cain"
       case "William \"Bill\" A. Callegari" | "William A. (Bill) Callegari" => "Bill Callegari"
       case "Ben D. Campbell" => "Ben Campbell"
+      case "Jaime L. Capelo" | "Jaime L. Capelo, Jr." => "Jaime Capelo"
       case "Bill G. Carter" => "Bill Carter"
       case "Warren D. Chisum" => "Warren Chisum"
       case "Garnet F. Coleman" => "Garnet Coleman"
       case "Karyne J. Conley" | "Karyne Jones Conley" => "Karyne Conley"
       case "John R. Cook" => "John Cook"
-      case "Robert (Robby) L. Cook" | "Robert L. (Robby) Cook" => "Robby Cook"
+      case "Robert (Robby) L. Cook" | "Robert Laird Cook, III" | "Robert L. (Robby) Cook" => "Robby Cook"
+      case "Frank J. Corte Jr." | "Frank Corte, Jr." | "Frank J. Corte, Jr." | "Frank Corte, Jr." => "Frank Corte"
       case "Thomas R. Craddick" => "Tom Craddick"
       case "C. Brandon Creighton" => "Brandon Creighton"
       case "Henry R. Cuellar" => "Henry Cuellar"
@@ -136,6 +191,7 @@ object TeliconCsvPrep {
       case "Bob Deuell" => "Robert Deuell"
       case "Dawnna Dukes" => "Dawna Dukes"
       case "Robert L. Duncan" => "Robert Duncan"
+      case "Harold Dutton Jr." | "Harold V. Dutton Jr." | "Harold V. Dutton, Jr." | "Harold Dutton, Jr." => "Harold Dutton"
       case "Robert Earley" => "Robert A. Earley"
       case "Harryette B. Ehrhardt" => "Harryette Ehrhardt"
       case "Dan P. Ellis" => "Dan Ellis"
@@ -149,7 +205,7 @@ object TeliconCsvPrep {
       case "Ismael \"Kino\" Flores" | "Ismael (Kino) Flores" => "Kino Flores"
       case "Stephen J. Frost" | "Stephen James Frost" => "Stephen Frost"
       case "Pete P. Gallego" => "Pete Gallego"
-      case "Mario V. Gallegos" => "Mario Gallegos"
+      case "Mario V. Gallegos, Jr." | "Mario V. Gallegos" | "Mario Gallegos, Jr." => "Mario Gallegos"
       case "Michael L. Galloway" => "Mike Galloway"
       case "Eddie De La Garza" => "Eddie de la Garza"
       case "Charlie Geren" => "Charles Geren"
@@ -160,25 +216,20 @@ object TeliconCsvPrep {
       case "Christopher (Chris) Harris" | "Christopher J. (Chris) Harris" => "Chris Harris"
       case "Will Ford Hartnett" => "Will Hartnett"
       case "Talmadge L. Heflin" => "Talmadge Heflin"
+      case "Glenn Hegar, Jr." => "Glenn Hegar"
       case "Ana E. Hernandez" => "Ana Hernandez"
+      case "Allen Ross Hightower, Jr." | "Allen Ross Hightower" => "Allen Hightower"
       case "Paul J. Hilbert" => "Paul Hilbert"
       case "Juan (Chuy) Hinojosa" => "Juan Hinojosa"
+      case "Ruben W. Hope, Jr." | "Ruben Hope, Jr." => "Ruben Hope"
       case "Charles (Chuck) Hopson" => "Chuck Hopson"
       case "Todd A. Hunter" => "Todd Hunter"
       case "Eddie Lucio, III" => "Eddie Lucio III"
-      case "Samuel William Hudson, III" => "Samuel W. Hudson, III"
+      case "Samuel William Hudson, III" | "Samuel W. Hudson, III" => "Samuel W. Hudson, III"
       case "Carl H. Isett" => "Carl Isett"
       case "James M. (Mike) Jackson" => "Mike Jackson"
       case "Delwin L. Jones" => "Delwin Jones"
       case "Jesse W. Jones" => "Jesse Jones"
-      case "Allen D. Place, Jr." => "Allen Place, Jr."
-      case "Curtis Lee Seidlits, Jr." => "Curtis L. Seidlits, Jr."
-      case "Eduardo A. (Eddie) Lucio, Jr." | "Eduardo (Eddie) Lucio, Jr." => "Eddie Lucio, Jr."
-      case "Frank J. Corte Jr." | "Frank J. Corte, Jr." => "Frank Corte, Jr."
-      case "Harold Dutton Jr." | "Harold V. Dutton Jr." | "Harold V. Dutton, Jr." => "Harold Dutton, Jr."
-      case "Mario V. Gallegos, Jr." => "Mario Gallegos, Jr."
-      case "Ruben W. Hope, Jr." => "Ruben Hope, Jr."
-      case "Solomon P. Ortiz, Jr." => "Solomon Ortiz, Jr."
       case "Robert A. Junell" => "Robert Junell"
       case "James L. (Jim) Keffer" => "Jim Keffer"
       case "William (Bill) Keffer" => "Bill Keffer"
@@ -187,15 +238,16 @@ object TeliconCsvPrep {
       case "Lois W. Kolkhorst" => "Lois Kolkhorst"
       case "James E. (Pete) Laney" | "James (Pete) E. Laney" => "James (Pete) Laney"
       case "David McQuade Leibowitz" => "David Leibowitz"
-      case "Ron E. Lewis" => "Ron Lewis"
       case "Ronald E. (Ron) Lewis" | "Ron E. Lewis" => "Ron Lewis"
       case "Jon S. Lindsay" => "Jon Lindsay"
       case "Elizabeth (Libby) Linebarger" => "Libby Linebarger"
       case "John Amos Longoria" | "John A. Longoria" => "John Longoria"
+      case "Eduardo A. (Eddie) Lucio, Jr." | "Eduardo (Eddie) Lucio, Jr." => "Eddie Lucio, Jr."
       case "Jerry A. Madden" => "Jerry Madden"
-      case "Frank Lloyd Madla" | "Frank L. Madla" => "Frank Madla"
+      case "Frank Lloyd Madla" | "Frank L. Madla, Jr." | "Frank L. Madla" => "Frank Madla"
       case "Ken E. Marchant" | "Kenny Marchant" => "Ken Marchant"
       case "Armando \"Mando\" Martinez" | "Armando (Mando) Martinez" => "Armando Alfonso Martinez"
+      case "Trey Martinez Fischer" => "Trey Martinez-Fischer"
       case "Ruth Jones McClendon" => "Ruth McClendon"
       case "Nancy H. McDonald" => "Nancy McDonald"
       case "Michael J. (Mike) Moncrief" => "Mike Moncrief"
@@ -209,13 +261,16 @@ object TeliconCsvPrep {
       case "Stephen E. (Steve) Ogden" | "Stephen Ogden" | "Steven Eugene Ogden" => "Steve Ogden"
       case "Rene O. Oliveira" => "Rene Oliveira"
       case "Dora F. Olivo" => "Dora Olivo"
+      case "Solomon P. Ortiz, Jr." => "Solomon Ortiz, Jr."
       case "Carl A. Parker" => "Carl Parker"
       case "Jerry E. Patterson" => "Jerry Patterson"
       case "L.P. (Pete) Patterson" => "L.P. Pete Patterson"
+      case "Allen D. Place, Jr." => "Allen Place, Jr."
       case "Joe C. Pickett" | "Joseph (Joe) C. Pickett" | "Joseph Pickett" => "Joe Pickett"
       case "Robert R. Puente" => "Robert Puente"
       case "Inocente (Chente) Quintanilla" | "Chente Quintanillia" => "Chente Quintanilla"
       case "Irma L. Rangel" => "Irma Rangel"
+      case "Aaron Pena, Jr." => "Aaron Pena"
       case "Richard Pena Raymond" | "Richard E. Raymond" => "Richard Raymond"
       case "Arthur C. (Art) Reyna" => "Arthur (Art) Reyna"
       case "Allan B. Ritter" | "Allen Ritter" => "Allan Ritter"
@@ -226,11 +281,13 @@ object TeliconCsvPrep {
       case "Paul L. Sadler" => "Paul Sadler"
       case "Sue A. Schechter" => "Sue Schechter"
       case "Eugene (Gene) T. Seaman" => "Gene Seaman"
+      case "Curtis Lee Seidlits, Jr." => "Curtis L. Seidlits, Jr."
       case "David M. Sibley" => "David Sibley"
       case "William M. (Bill) Sims" => "Bill Sims"
       case "John H. Shields" => "John Shields"
       case "Burt R. Solomons" | "Burt P. Solomons" => "Burt Solomons"
       case "Mark W. Stiles" => "Mark Stiles"
+      case "Joe R. Straus, III" => "Joe Straus"
       case "David A. Swinford" => "David Swinford"
       case "Robert E. Talton" => "Robert Talton"
       case "Barry B. Telford" => "Barry Telford"
@@ -268,8 +325,8 @@ object TeliconCsvPrep {
       case "11/ /39" => dateString
       case "11/07/" => dateString
       case "11/09" => dateString
-      case "6-29" =>
-      case "02265430" => dateString
+      case "6-29" => dateString
+      case "02265430" => "2/26/1954"
       case "Dec. 27" | "12/27/" => "12/27"
       case DateRe(m, _, d, _, y, _) =>
         val yi = y.toInt
